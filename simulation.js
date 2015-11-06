85a1d0f1129144d6c774e6a7d7e7118e7dd52b61
hedb <hed.barnissan@gmail.com>
2015-11-07 00:51:34 +0200

    Completing writing the GameOfLife step

--- simulation.js
+++ simulation.js
@@ -9,7 +9,22 @@ var Simulation = {
 			agent.forEach_8_Neighbours(function (neighbour) {
 				altGrid.augment(neighbour.x,neighbour.y,1);
 			});
-		});		
+		});
+
+		world.forEachAgent(function (agent) {
+			var neigbourCount = altGrid.get(agent.x,agent.y);
+			if (neigbourCount != 2 && neigbourCount != 3) {
+				world.removeAgent(agent);
+			}
+		});
+
+		altGrid.forEachEntry(function (x,y,neigbourCount) {
+			if (neigbourCount == 2 || neigbourCount == 3) {
+				if (world.getAgent(x,y) == null) {
+					world.addAgentsToWorld({x:x,y:y});
+				}
+			}		
+		});
 	}
 
 };
