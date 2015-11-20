ef619063db8575b2aa2cffaa6721fb5549988104
hedb <hed.barnissan@gmail.com>
2015-11-20 11:19:05 +0200

    Pushed everything to GUI configuration

--- simulation.js
+++ simulation.js
@@ -26,6 +26,34 @@ var Simulation = {
 				}
 			}		
 		});
+	},
+
+
+	moveRandomly:function () {
+
+		_.each(world.agents,function(agent) {
+			if (agent!=null) {
+					var xDiff = (Math.random()>.5) ,
+						yDiff = 1- xDiff,
+						sign = (Math.random()>.5) ?1:-1;
+
+					world.moveAgent(xDiff*sign,yDiff*sign,agent);
+				}
+			}
+		)
+	},
+
+
+	randStartConf:function(n,squareSize) {
+		var ret = [];
+		for (var i = 0; i < n; i++) {
+			ret.push(new Agent(
+							squareSize-Math.floor(Math.random()*squareSize*2),
+							squareSize-Math.floor(Math.random()*squareSize*2)
+							)
+			);
+		}
+		return ret;
 	}
 
 };
